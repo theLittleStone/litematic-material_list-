654ff5c6001d5d860d5bbbc4983c12e71fc9e468
theLittleStone <67592979+theLittleStone@users.noreply.github.com>
2021-10-12 09:12:19 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -36,62 +36,84 @@ for path in file_paths:
     excelSheet.col(3).width = 17 * 256
 
     # 设置单元格格式
-    font1 = xlwt.Font()
-    font1.bold = True
-    font2 = font1
-    font1.height = 20 * 14  # 11是字号
+    font0 = xlwt.Font()
+    font0.bold = True
+    font3 = font0
+    font0.height = 20 * 14  # 11是字号
+
+    # 白色背景
+    pattern1 = xlwt.Pattern()
+    pattern1.pattern = xlwt.Pattern.SOLID_PATTERN
+    pattern1.pattern_fore_colour = 1
+    # 灰色背景
+    pattern2 = xlwt.Pattern()
+    pattern2.pattern = xlwt.Pattern.SOLID_PATTERN
+    pattern2.pattern_fore_colour = 67
 
     # 居中, 加粗, 放大
+    style0 = xlwt.XFStyle()
+    a0 = xlwt.Alignment()
+    a0.horz = a0.HORZ_CENTER
+    style0.alignment = a0
+    style0.font = font0
+    style0.pattern = pattern2
+
     style1 = xlwt.XFStyle()
-    a1 = xlwt.Alignment()
-    a1.horz = a1.HORZ_CENTER
-    style1.alignment = a1
-    style1.font = font1
-
-    # 加粗, 不居中
     style2 = xlwt.XFStyle()
-    style2.font = font2
+    style1.pattern = pattern1
+    style2.pattern = pattern2
+    # 加粗, 不居中
+    style3 = xlwt.XFStyle()
+    style3.font = font3
 
-    contentLines = f.readlines()
-
+    try:
+        contentLines = f.readlines()
+    except:
+        print(1)
     isTitleLine = True
     excelLine = 1
     styleIsRight = False
     for line in contentLines:
+        # 每行背景色
+        if excelLine % 2 == 0:
+            style = style2
+        else:
+            style = style1
+
         if line.startswith('+--'):
             styleIsRight = True
             continue
         elif isTitleLine and styleIsRight:
             title = line
             title = re.findall('[|]\s*(.*?)\s*[|]', title)
-            excelSheet.write_merge(0, 0, 0, 3, title, style1)
+            excelSheet.write_merge(0, 0, 0, 3, title, style0)
             isTitleLine = False
         elif styleIsRight:
             strList = re.findall('[|]\s*(.*?)\s*[|]\s*(.*?)\s*[|]', line)
             st = strList[0]
             if st[1].isdigit():
                 materialNum = int(st[1])
-                excelSheet.write(excelLine, 0, st[0])
-                excelSheet.write(excelLine, 1, st[1])
+                excelSheet.write(excelLine, 0, st[0], style)
+                excelSheet.write(excelLine, 1, st[1], style)
                 if materialNum >= 64:  # 算组数
                     if materialNum % 64 == 0:
-                        excelSheet.write(excelLine, 2, (str(materialNum // 64) + " set(s)"))
+                        excelSheet.write(excelLine, 2, (str(materialNum // 64) + " set(s)"), style)
                     else:
-                        excelSheet.write(excelLine, 2, (str(materialNum // 64) + " set(s) + " + str(materialNum % 64)))
+                        excelSheet.write(excelLine, 2, (str(materialNum // 64) + " set(s) + " + str(materialNum % 64)), style)
                     if materialNum > (64 * 27):
-                        excelSheet.write(excelLine, 3, str(materialNum // (64 * 27), ) + ' box(s) +' + str((materialNum % (64 * 27)) // 64 + 1) + " set(s)")
+                        excelSheet.write(excelLine, 3, str(materialNum // (64 * 27), ) + ' box(s) +' + str((materialNum % (64 * 27)) // 64 + 1) + " set(s)", style)
                     else:
-                        excelSheet.write(excelLine, 3, "-----------")
+                        excelSheet.write(excelLine, 3, "----------", style)
                 else:
-                    excelSheet.write(excelLine, 2, "< 1")
-                    excelSheet.write(excelLine, 3, "-----------")
+                    excelSheet.write(excelLine, 2, "----------", style)
+                    excelSheet.write(excelLine, 3, "----------", style)
                 # 算盒数
             else:
                 materialNum = 0
-                excelSheet.write(excelLine, 2, "SetNum", style2)
-                excelSheet.write(excelLine, 3, "BoxNum", style2)
-                excelSheet.write(excelLine, 1, st[1], style2)
-                excelSheet.write(excelLine, 0, st[0], style2)
+                excelSheet.write(excelLine, 2, "SetNum", style3)
+                excelSheet.write(excelLine, 3, "BoxNum", style3)
+                excelSheet.write(excelLine, 1, st[1], style3)
+                excelSheet.write(excelLine, 0, st[0], style3)
             excelLine += 1
 
     if not styleIsRight:
@@ -102,6 +124,8 @@ for path in file_paths:
             excelFile.save(path + ".xls")
         except:
             print(path + "保存文件失败, 文件可能在别处被打开")
+            continue
+        print(path + '转换完成')
     f.close()
-    print(path + '转换完成')
+
 time.sleep(1.5)
